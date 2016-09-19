8767b5832d1f0c4c92825909a01a0b5c46aec989
Rajeeb Saha <rajeeb.saha@aquaticinformatics.com>
2016-09-19 21:35:08 +0000

    Enabled attribute routing. Added AdminLTE template. Added bootstrap, font-awesome, iCheck, pace, jquery, jquery input mask.
    TFS Changeset 107: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/107 
    
    Workitems:
    [339] Project Setup
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=339 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~339

--- Linko.LinkoExchange.Web/App_Start/RouteConfig.cs
+++ Linko.LinkoExchange.Web/App_Start/RouteConfig.cs
@@ -13,6 +13,9 @@ namespace Linko.LinkoExchange.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //Enable attribute routing.
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
