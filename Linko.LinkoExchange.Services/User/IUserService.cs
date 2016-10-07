ff50bc9787ecc0f6ed3e1eb461d2b73a9b27a8ec
Markus Jeon <WATERTRAX\mjeon>
2016-10-07 22:36:54 +0000

    Missed this one before binding!
    TFS Changeset 119: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/119 
    
    Workitems:
    [650] Coding - Markus Use Case Tasks
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=650 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~650

---
+++ Linko.LinkoExchange.Services/User/IUserService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linko.LinkoExchange.Services.User
+{
+    public class UserProfileDTO
+    {
+        public int UserProfileId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string TitleRole { get; set; }
+        public string BusinessName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string CityName { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PhoneExt { get; set; }
+    }
+
+    public class QuestionAnswerPair
+    {
+        public int QuestionId { get; set; }
+        public string Question { get; set; }
+        public string AnswerId { get; set; }
+        public string Answer { get; set; }
+    }
+
+    public class UserQuestionAnswerPairsDTO
+    {
+        public List<QuestionAnswerPair> QuestionAnswerPairs { get; set; }
+    }
+
+    public enum UserEmailCommunicationType
+    {
+        AuthorityInvitation,
+        AuthorityRegistrationApproved,
+        AuthorityRegistrationDenied,
+        AuthorityReset,
+        AuthorityAccountReset,
+        AuthorityEmailChanged,
+        AuthorityToIndustryInvitation,
+        AuthorityToIndustryRegistrationApproved,
+        AuthorityToIndustryRegistrationDenied,
+        IndustryToIndustryInvitation,
+        IndustryToIndustryRegistrationApproved,
+        IndustryToIndustryRegistrationDenied,
+        
+    }
+
+    public interface IUserService
+    {
+        UserProfileDTO GetUserProfileById(int userProfileId);
+
+        UserProfileDTO GetUserProfileByEmail(string emailAddress);
+
+        List<UserProfileDTO> GetUserProfilesForOrgRegProgram(int organizationRegulatoryProgramId,
+                             bool? isRegApproved,
+                             bool? isRegDenied,
+                             bool? isEnabled,
+                             bool? isRemoved);
+
+        void SendEmailCommunication(int userProfileId, UserEmailCommunicationType type, string emailAddressOverride = null);
+
+        int AddNewUser(string emailAddress, string firstName, string lastName);
+
+        void UpdateUserProfileState(int userProfileId, bool? isRegApproved, bool? isRegDenied, bool? isEnabled, bool? isRemoved);
+
+        void UpdateUserPermissionGroupId(int userProfileId, int permissionGroupId);
+
+        void UpdateUserSignatoryStatus(bool isSignatory);
+
+        void RequestSignatoryStatus(int userProfileId);
+
+        void ResetUser(int userProfileId);
+
+        void RemoveUser(int userProfileId);
+
+        void UpdateUserProfile(UserProfileDTO request);
+
+        void ChangePassword(int userProfileID, string oldPassword, string newPassword);
+
+        void UpdateQuestionAnswerPairs(UserQuestionAnswerPairsDTO questionAnswerPairs);
+
+    }
+}
