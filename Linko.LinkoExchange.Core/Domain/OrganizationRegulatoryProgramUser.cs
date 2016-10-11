e955d0543e9f49724328c91eca3cf80e471f5115
Markus Jeon <WATERTRAX\mjeon>
2016-10-11 16:40:16 +0000

    Added OrganizationRegulatoryProgramUser POCO
    TFS Changeset 126: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/126 
    
    Workitems:
    [650] Coding - Markus Use Case Tasks
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=650 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~650

---
+++ Linko.LinkoExchange.Core/Domain/OrganizationRegulatoryProgramUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linko.LinkoExchange.Core.Domain
+{
+    public class OrganizationRegulatoryProgramUser
+    {
+        public int OrganizationRegulatoryProgramUserId { get; set; }
+        public bool IsRegistrationApproved { get; set; }
+        public bool IsRegistrationDenied { get; set; }
+        public bool IsEnabled { get; set; }
+        public bool IsRemoved { get; set; }
+        public bool IsSignatory { get; set; }
+        public DateTime LastModificationDateTime { get; set; }
+        public int LastModificationUserId { get; set; }
+}
