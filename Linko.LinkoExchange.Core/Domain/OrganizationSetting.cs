789dd0397273b3f714af01a6e1023323073d3bec
Markus Jeon <WATERTRAX\mjeon>
2016-10-11 18:09:24 +0000

    Quick fix of code to allow Shuhao to run/test app for login. Added key to OrganizationSetting and removed OnModelCreating override in ApplicaitonDbContext.
    TFS Changeset 130: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/130 
    
    Workitems:
    [650] Coding - Markus Use Case Tasks
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=650 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~650

--- Linko.LinkoExchange.Core/Domain/OrganizationSetting.cs
+++ Linko.LinkoExchange.Core/Domain/OrganizationSetting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,9 @@ namespace Linko.LinkoExchange.Core.Domain
 {
     public class OrganizationSetting
     {
+        [Key]
+        public int OrganizationSettingId { get; set;}
+        public int OrganizationId { get; set;}
         public string Name { get; set;}
         public string Value { get; set;}
     }
--- Linko.LinkoExchange.Data/ApplicationDbContext.cs
+++ Linko.LinkoExchange.Data/ApplicationDbContext.cs
@@ -28,19 +28,5 @@ namespace Linko.LinkoExchange.Data
         public DbSet<OrganizationRegulatoryProgramUser> OrganizationRegulatoryProgramUsers { get; set; }
         
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            var typesToRegister = Assembly.GetExecutingAssembly().GetTypes()
-                                                                .Where(type => !String.IsNullOrEmpty(type.Namespace))
-                                                                .Where(type => type.BaseType != null && type.BaseType.IsGenericType &&
-                                                                               type.BaseType.GetGenericTypeDefinition() == typeof(EntityTypeConfiguration<>));
-            foreach (var type in typesToRegister)
-            {
-                dynamic configurationInstance = Activator.CreateInstance(type);
-                modelBuilder.Configurations.Add(configurationInstance);
-            }
-
-           
-        }
     }
 }
