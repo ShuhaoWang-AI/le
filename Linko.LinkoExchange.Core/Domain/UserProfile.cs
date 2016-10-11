495809fb94cd25b475e3f2e96fce7b4f8a4d19d7
Markus Jeon <WATERTRAX\mjeon>
2016-10-11 15:30:16 +0000

    Added POCO rough-in for UserProfile
    TFS Changeset 121: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/121 
    
    Workitems:
    [650] Coding - Markus Use Case Tasks
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=650 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~650

---
+++ Linko.LinkoExchange.Core/Domain/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linko.LinkoExchange.Core.Domain
+{
+    public class UserProfile
+    {
+        public int UserProfileId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
