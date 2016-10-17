4ed5e6b26099227824148e3f3b5060837a5a0fb1
Shuhao Wang <shuhao.wang@aquaticinformatics.com>
2016-10-17 17:52:19 +0000

    invitation mappping
    TFS Changeset 162: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/162 
    
    Workitems:
    [656] Coding - Implement Email Service
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=656 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~656

---
+++ Linko.LinkoExchange.Core/Domain/Invitation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Linko.LinkoExchange.Core.Domain
+{
+    public class Invitation
+    {
+        [Key]
+        public int InvitationId { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime InvitationDateTimeUtc { get; set; }
+        public int SenderOrganizationRegulatoryProgramId { get; set; }
+        public int RecipientOrganizationRegulatoryProgramId { get; set; } 
+    } 
+}
---
+++ Linko.LinkoExchange.Data/Mapping/InvitationMap.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using Linko.LinkoExchange.Core.Domain;
+
+namespace Linko.LinkoExchange.Data.Mapping
+{
+    class InvitationMap : EntityTypeConfiguration<Invitation>
+    {
+        public InvitationMap()
+        {
+            ToTable("tInvitation");
+
+            HasKey(i => i.InvitationId);
+
+            Property(i => i.FirstName);
+            Property(i => i.LastName);
+            Property(i => i.EmailAddress).IsRequired();
+            Property(i => i.InvitationDateTimeUtc).IsRequired();
+            Property(i => i.SenderOrganizationRegulatoryProgramId).IsRequired();
+            Property(i => i.RecipientOrganizationRegulatoryProgramId).IsRequired(); 
+        }
+    }
+}
