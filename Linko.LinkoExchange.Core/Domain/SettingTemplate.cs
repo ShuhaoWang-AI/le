bc045b0401296f36b25c82757117853e5590d161
Markus Jeon <WATERTRAX\mjeon>
2016-10-18 21:17:26 +0000

    Added new POCO
    TFS Changeset 175: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/175 
    
    Workitems:
    [650] Coding - Markus Use Case Tasks
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=650 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~650

---
+++ Linko.LinkoExchange.Core/Domain/SettingTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linko.LinkoExchange.Core.Domain
+{
+    public class SettingTemplate
+    {
+        [Key]
+        public int SettingTemplateId { get; set; }
+        public string Name { get; set; }
+    }
+}
