cc00b0f35f34a53e14cf3a7fe38bf40395ddfe0b
Markus Jeon <WATERTRAX\mjeon>
2016-10-19 16:05:08 +0000

    Added test project and basic OrganizationService test
    TFS Changeset 188: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/188 
    
    Workitems:
    [650] Coding - Markus Use Case Tasks
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=650 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~650

---
+++ Linko.LinkoExchange.Test/OrganizationServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Linko.LinkoExchange.Services;
+using Linko.LinkoExchange.Data;
+using AutoMapper;
+using Linko.LinkoExchange.Services.AutoMapperProfile;
+
+namespace Linko.LinkoExchange.Test
+{
+    [TestClass]
+    public class OrganizationServiceTests
+    {
+        private OrganizationService orgService;
+        private const string CONN_STRING = "Integrated Security=SSPI;Initial Catalog=LXDev01;Data Source=(local);";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.AddProfile(new UserMapProfile());
+                cfg.AddProfile(new EmailAuditLogEntryMapProfile());
+                cfg.AddProfile(new InvitationMapProfile());
+                cfg.AddProfile(new OrganziationRegulatoryProgramUserDtoMapProfile());
+                cfg.AddProfile(new OrganizationMapProfile());
+            });
+
+            //Make sure there no methods were missing in the mappings loaded above via profiles
+            Mapper.AssertConfigurationIsValid();
+
+            orgService = new OrganizationService(new LinkoExchangeContext(CONN_STRING), Mapper.Instance);
+            var org = orgService.GetOrganization(1);
+
+        }
+    }
+}
