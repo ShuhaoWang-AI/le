7173a9e9754a8d476c1df4e1a0af65951a3f408d
Markus Jeon <WATERTRAX\mjeon>
2016-10-21 17:13:37 +0000

    InvitationDateTimeUtc -> DateTimeOffset
    TFS Changeset 220: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/220 
    
    Workitems:
    [650] Coding - Markus Use Case Tasks
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=650 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~650

--- Linko.LinkoExchange.Services/Dto/InvitationDto.cs
+++ Linko.LinkoExchange.Services/Dto/InvitationDto.cs
@@ -8,7 +8,7 @@ namespace Linko.LinkoExchange.Services.Dto
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
-        public DateTime InvitationDateTimeUtc { get; set; }
+        public DateTimeOffset InvitationDateTimeUtc { get; set; }
         public int SenderOrganizationRegulatoryProgramId { get; set; }
         public int RecipientOrganizationRegulatoryProgramId { get; set; }
     }
