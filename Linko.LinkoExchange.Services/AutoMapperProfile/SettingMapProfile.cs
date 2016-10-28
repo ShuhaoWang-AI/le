5a54b8366c1f2614162bffd6033ef90081f09799
Shuhao Wang <shuhao.wang@aquaticinformatics.com>
2016-10-28 23:06:54 +0000

    Fix setting service mapping issue
    TFS Changeset 271: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/271 
    
    Workitems:
    [655] Coding - UC-29: "Login"
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=655 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~655

--- Linko.LinkoExchange.Services/AutoMapperProfile/SettingMapProfile.cs
+++ Linko.LinkoExchange.Services/AutoMapperProfile/SettingMapProfile.cs
@@ -14,10 +14,15 @@ namespace Linko.LinkoExchange.Services.AutoMapperProfile
         public SettingMapProfile()
         {
             CreateMap<OrganizationSetting, SettingDto>()
-               .ForMember(d => d.Type, o => o.MapFrom(s => s.SettingTemplate.Name));
+             .ForMember(d => d.Type, op => op.ResolveUsing(o => MapSettingType(o.SettingTemplate.Name))); 
 
             CreateMap<OrganizationRegulatoryProgramSetting, SettingDto>()
                .ForMember(d => d.Type, o => o.MapFrom(s => s.SettingTemplate.Name));
+        }
+
+        public static SettingType MapSettingType(string settingType)
+        {
+            return (SettingType)Enum.Parse(typeof(SettingType), settingType);
         }
     }
 }
