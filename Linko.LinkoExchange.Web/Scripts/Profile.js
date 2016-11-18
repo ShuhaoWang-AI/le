7fdb14afd7c22b87b668ad07e5e603542182dbd5
Shuhao Wang <shuhao.wang@aquaticinformatics.com>
2016-11-18 21:59:25 +0000

    1 Fix check->minus issue 2 Make the post page focus on the div posted to
    TFS Changeset 447: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/447 
    
    Workitems:
    [1157] UC-30 manager user profile
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=1157 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~1157

--- Linko.LinkoExchange.Web/Scripts/Profile.js
+++ Linko.LinkoExchange.Web/Scripts/Profile.js
@@ -8,23 +8,23 @@ $(document).ready(function () {
     $("#kbq-panel input[type='text'").hide();
 
     $(".box-primary").addClass("collapsed-box");
-    $(".box-primary").find("i.fa.fa-minus").removeClass('fa-minus');
-    $(".box-primary").find("i.fa").addClass('fa-plus');
+    $(".box-primary >.box-header").find("i.fa.fa-minus").removeClass('fa-minus');
+    $(".box-primary >.box-header").find("i.fa").addClass('fa-plus');
 
     if (profileCollapsed.toLowerCase() == 'false') {
         $("#user-info-panel .box-primary").removeClass("collapsed-box");
-        $("#user-info-panel .box-primary").find("i.fa.fa-plus").removeClass('fa-plus');
-        $("#user-info-panel .box-primary").find("i.fa").addClass('fa-minus');
+        $("#user-info-panel .box-primary >.box-header").find("i.fa.fa-plus").removeClass('fa-plus');
+        $("#user-info-panel .box-primary >.box-header").find("i.fa").addClass('fa-minus');
     }
     if (kbqCollapsed.toLowerCase() == 'false') {
         $("#kbq-panel .box-primary").removeClass("collapsed-box");
-        $("#kbq-panel .box-primary").find("i.fa.fa-plus").removeClass('fa-plus');
-        $("#kbq-panel .box-primary").find("i.fa").addClass('fa-minus');
+        $("#kbq-panel .box-primary >.box-header").find("i.fa.fa-plus").removeClass('fa-plus');
+        $("#kbq-panel .box-primary >.box-header ").find("i.fa").addClass('fa-minus');
     }
     if (sqCollapsed.toLowerCase() == "false") {
         $("#sq-panel .box-primary").removeClass("collapsed-box");
-        $("#sq-panel .box-primary").find("i.fa.fa-plus").removeClass('fa-plus');
-        $("#sq-panel .box-primary").find("i.fa").addClass('fa-minus');
+        $("#sq-panel .box-primary >.box-header").find("i.fa.fa-plus").removeClass('fa-plus');
+        $("#sq-panel .box-primary >.box-header").find("i.fa").addClass('fa-minus');
     }
   
     $(document).find("#kbq-panel select").on("change", function () {
