28597e5928d6a565cff9d8f949d48871050aec84
Shuhao Wang <shuhao.wang@aquaticinformatics.com>
2017-03-20 23:11:55 +0000

    Fix LastModificationDatetime issue
    TFS Changeset 742: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/742 
    
    Workitems:
    [2226] Coding : General implementation task for Iteration 2 code
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=2226 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~2226

--- Linko.LinkoExchange.Services/Dto/ReportPackageTemplateDto.cs
+++ Linko.LinkoExchange.Services/Dto/ReportPackageTemplateDto.cs
@@ -16,9 +16,6 @@ namespace Linko.LinkoExchange.Services.Dto
         public CtsEventTypeDto CtsEventType { get; set; }
         public int OrganizationRegulatoryProgramId { get; set; }
         public bool IsActive { get; set; }
-
-        //public DateTimeOffset LastModificationDateTimeUtc { get; set; }
-        public string CreateDateTimeOrLastmodificiationDateTime { get; set; }
         public DateTime LastModificationDateTimeLocal { get; set; }
 
         public int? LastModifierUserId { get; set; }
