ab7a356c8545de9dbd081f2cfdd9125c6a4e1994
Shuhao Wang <shuhao.wang@aquaticinformatics.com>
2017-03-27 17:40:09 +0000

    Add attachment service
    TFS Changeset 782: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/782 
    
    Workitems:
    [2419] Code task for milestone 2
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=2419 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~2419

---
+++ Linko.LinkoExchange.Services/Attachment/AttachmentService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Linko.LinkoExchange.Services.Mapping;
+
+namespace Linko.LinkoExchange.Services.Attachment
+{
+    public class AttachmentService : IAttachmentService
+    {
+        private readonly DbContext _dbContext;
+        private readonly IMapHelper _mapHelper;
+
+        private readonly string[] _validExtensions =
+        {
+            "docx", "doc", "xls", "xlsx", "pdf", "tif",
+            "jpg", "jpeg", "bmp", "png", "txt", "csv"
+        };
+
+        public AttachmentService(
+            DbContext dbContext,
+            IMapHelper mapHelper)
+        {
+            _dbContext = dbContext;
+            _mapHelper = mapHelper;
+        }
+
+        public List<string> GetValidAttachmentFileExtensions()
+        {
+            return new List<string>(_validExtensions);
+        }
+
+        public bool IsValidFileExtension(string ext)
+        {
+            if (string.IsNullOrWhiteSpace(ext))
+            {
+                return false;
+            }
+
+            if (ext.StartsWith("."))
+            {
+                ext = ext.Substring(1);
+            }
+
+            return _validExtensions.Contains(ext);
+        }
+
+        public IList<string> GetUserAttachmentFiles()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Linko.LinkoExchange.Services/Attachment/IAttachmentService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Linko.LinkoExchange.Services.Attachment
+{
+    public interface IAttachmentService
+    {
+        List<string> GetValidAttachmentFileExtensions();
+        bool IsValidFileExtension(string ext);
+
+
+        /// <summary>
+        /// Get current user's (identified by OrgRegProgremId) attachment files list
+        /// </summary>
+        /// <returns></returns>
+        IList<string> GetUserAttachmentFiles();
+    }
+}
