862c0d8123be338701082ddcfdf454959a72ff28
Markus Jeon <WATERTRAX\mjeon>
2017-04-12 20:06:15 +0000

    Comments added to each method in Report Element Service methods (interface and implementation)
    TFS Changeset 891: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/891 
    
    Workitems:
    [2419] Code task for milestone 2
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=2419 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~2419

--- Linko.LinkoExchange.Services/Report/IReportElementService.cs
+++ Linko.LinkoExchange.Services/Report/IReportElementService.cs
@@ -10,10 +10,39 @@ namespace Linko.LinkoExchange.Services.Report
 {
     public interface IReportElementService
     {
+        /// <summary>
+        /// Gets a collection of Report Element Types associated with a category.
+        /// </summary>
+        /// <param name="categoryName">The name of the report element category</param>
+        /// <returns>Collection of dto's that map to the Report Element Type objects associated with the passed in category </returns>
         IEnumerable<ReportElementTypeDto> GetReportElementTypes(ReportElementCategoryName categoryName);
+
+        /// <summary>
+        /// Gets the details of a Report Element Type from the database.
+        /// </summary>
+        /// <param name="reportElementTypeId">ReportElementTypeId in the tReportElementType table</param>
+        /// <returns>Dto that maps to the Report Element Type object associated with the passed in Id</returns>
         ReportElementTypeDto GetReportElementType(int reportElementTypeId);
+
+        /// <summary>
+        /// Creates a new Report Element Type in the database or updates an existing one (if an Id is provided)
+        /// </summary>
+        /// <param name="reportElementType">The Dto that gets mapped to a Report Element Type and saved to the DB.
+        /// If and Id is not provided, it is assumed a new object gets created in the database.</param>
+        /// <returns>Existing Id or newly created Id</returns>
         int SaveReportElementType(ReportElementTypeDto reportElementType);
+
+        /// <summary>
+        /// Removes a Report Element Type from the database
+        /// </summary>
+        /// <param name="reportElementTypeId">Id of the Report Element Type to remove from the database</param>
         void DeleteReportElementType(int reportElementTypeId);
+
+        /// <summary>
+        /// Checks to see if a Report Element Type is used in any Report Package Template
+        /// </summary>
+        /// <param name="reportElementTypeId">The Id of the Report Element Type to check.</param>
+        /// <returns>True = Report Element Type is included in at least 1 Report Package Template, False otherwise.</returns>
         bool IsReportElementTypeInUse(int reportElementTypeId);
     }
 }
--- Linko.LinkoExchange.Services/Report/ReportElementService.cs
+++ Linko.LinkoExchange.Services/Report/ReportElementService.cs
@@ -41,6 +41,11 @@ namespace Linko.LinkoExchange.Services.Report
             _timeZoneService = timeZoneService;
         }
 
+        /// <summary>
+        /// Gets a collection of Report Element Types associated with a category.
+        /// </summary>
+        /// <param name="categoryName">The name of the report element category</param>
+        /// <returns>Collection of dto's that map to the Report Element Type objects associated with the passed in category </returns>
         public IEnumerable<ReportElementTypeDto> GetReportElementTypes(ReportElementCategoryName categoryName)
         {
             _logger.Info($"Enter ReportElementService.GetReportElementTypes. categoryName={categoryName}");
@@ -81,6 +86,11 @@ namespace Linko.LinkoExchange.Services.Report
             return reportElementTypes;
         }
 
+        /// <summary>
+        /// Gets the details of a Report Element Type from the database.
+        /// </summary>
+        /// <param name="reportElementTypeId">ReportElementTypeId in the tReportElementType table</param>
+        /// <returns>Dto that maps to the Report Element Type object associated with the passed in Id</returns>
         public ReportElementTypeDto GetReportElementType(int reportElementTypeId)
         {
             _logger.Info($"Enter ReportElementService.GetReportElementType. reportElementTypeId={reportElementTypeId}");
@@ -116,6 +126,12 @@ namespace Linko.LinkoExchange.Services.Report
             return dto;
         }
 
+        /// <summary>
+        /// Creates a new Report Element Type in the database or updates an existing one (if an Id is provided)
+        /// </summary>
+        /// <param name="reportElementType">The Dto that gets mapped to a Report Element Type and saved to the DB.
+        /// If and Id is not provided, it is assumed a new object gets created in the database.</param>
+        /// <returns>Existing Id or newly created Id</returns>
         public int SaveReportElementType(ReportElementTypeDto reportElementType)
         {
             string reportElementTypeIdString = string.Empty;
@@ -270,7 +286,10 @@ namespace Linko.LinkoExchange.Services.Report
             return reportElementTypeIdToReturn;
         }
 
-    
+        /// <summary>
+        /// Removes a Report Element Type from the database
+        /// </summary>
+        /// <param name="reportElementTypeId">Id of the Report Element Type to remove from the database</param>
         public void DeleteReportElementType(int reportElementTypeId)
         {
             _logger.Info($"Enter ReportElementService.DeleteReportElementType. reportElementTypeId={reportElementTypeId}");
@@ -338,6 +357,11 @@ namespace Linko.LinkoExchange.Services.Report
 
         }
 
+        /// <summary>
+        /// Checks to see if a Report Element Type is used in any Report Package Template
+        /// </summary>
+        /// <param name="reportElementTypeId">The Id of the Report Element Type to check.</param>
+        /// <returns>True = Report Element Type is included in at least 1 Report Package Template, False otherwise.</returns>
         public bool IsReportElementTypeInUse(int reportElementTypeId)
         {
             bool isInUse;
