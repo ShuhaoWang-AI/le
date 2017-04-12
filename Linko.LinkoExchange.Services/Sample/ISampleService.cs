92ec144c2aac1a62ff525146b881b121d4c2efd1
Markus Jeon <WATERTRAX\mjeon>
2017-04-12 15:41:00 +0000

    Added comments to Sample Service interface methods.
    TFS Changeset 883: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/883 
    
    Workitems:
    [2419] Code task for milestone 2
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=2419 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~2419

--- Linko.LinkoExchange.Services/Sample/ISampleService.cs
+++ Linko.LinkoExchange.Services/Sample/ISampleService.cs
@@ -10,11 +10,53 @@ namespace Linko.LinkoExchange.Services.Sample
 {
     public interface ISampleService
     {
+        /// <summary>
+        /// Gets Samples from the database for displaying in a grid
+        /// </summary>
+        /// <param name="status">SampletStatus type to filter by</param>
+        /// <param name="startDate">Nullable localized date/time time period range. 
+        /// Sample start dates must on or after this date/time. Null parameters are ignored and not part of the filter.</param>
+        /// <param name="endDate">Nullable localized date/time time period range. 
+        /// Sample end dates must on or before this date/time. Null parameters are ignored and not part of the filter.</param>
+        /// <returns>Collection of filtered Sample Dto's</returns>
         IEnumerable<SampleDto> GetSamples(SampleStatusName status, DateTime? startDate = null, DateTime? endDate = null);
+
+        /// <summary>
+        /// Saves a Sample to the database after validating. Throw a list of RuleViolation exceptions
+        /// for failed validation issues.
+        /// </summary>
+        /// <param name="sample">Sample Dto</param>
+        /// <param name="isSavingAsReadyToSubmit">True to perform stricter validation</param>
+        /// <returns>Existing Sample Id or newly created Sample Id</returns>
         int SaveSample(SampleDto sample, bool isSavingAsReadyToSubmit = false);
+
+        /// <summary>
+        /// Deletes a sample from the database
+        /// </summary>
+        /// <param name="sampleId">SampleId associated with the object in the tSample table</param>
         void DeleteSample(int sampleId);
+
+        /// <summary>
+        /// Gets the complete details of a single Sample
+        /// </summary>
+        /// <param name="sampleId">SampleId associated with the object in the tSample table</param>
+        /// <returns>Sample Dto associated with the passed in Id</returns>
         SampleDto GetSampleDetails(int sampleId);
+
+        /// <summary>
+        /// Test to see if a Sample is included in at least 1 report package
+        /// </summary>
+        /// <param name="sampleId">SampleId associated with the object in the tSample table</param>
+        /// <returns>Boolean indicating if the Sample is or isn't included in at least 1 report package</returns>
         bool IsSampleIncludedInReportPackage(int sampleId);
+
+        /// <summary>
+        /// Tests validation of a passed in Sample in either Draft or ReadyToReport Mode
+        /// </summary>
+        /// <param name="sampleDto">Sample to validate</param>
+        /// <param name="isReadyToSubmit">False = Draft Mode, True = ReadyToReport Mode</param>
+        /// <param name="isSuppressExceptions">False = throws RuleViolation exception, True = does not throw RuleViolation exceptions</param>
+        /// <returns>Boolean indicating if Sample passed all validation (Draft or ReadyToReport mode)</returns>
         bool IsValidSample(SampleDto sampleDto, bool isReadyToSubmit, bool isSuppressExceptions = false);
     }
 }
