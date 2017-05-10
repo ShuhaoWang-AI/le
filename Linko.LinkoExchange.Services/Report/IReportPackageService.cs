a3c7fe685a507d27f4304cce35bdc4eebb098170
Rajeeb Saha <rajeeb.saha@aquaticinformatics.com>
2017-05-10 21:58:46 +0000

    Code Clean up.
    TFS Changeset 1043: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/1043 
    
    Workitems:
    [3132] Coding - UI - User Story 2127:STORY: UC-16 Compile Report Package
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=3132 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~3132

--- Linko.LinkoExchange.Services/Report/IReportPackageService.cs
+++ Linko.LinkoExchange.Services/Report/IReportPackageService.cs
@@ -1,7 +1,7 @@
-using System.Collections.Generic;
-using Linko.LinkoExchange.Services.Dto;
 using System;
+using System.Collections.Generic;
 using Linko.LinkoExchange.Core.Enum;
+using Linko.LinkoExchange.Services.Dto;
 
 namespace Linko.LinkoExchange.Services.Report
 {
@@ -13,60 +13,59 @@ namespace Linko.LinkoExchange.Services.Report
         CopyOfRecordValidationResultDto VerififyCopyOfRecord(int reportPackageId);
 
         /// <summary>
-        /// Get ReportPackageDto object by report package Id. 
+        ///     Get ReportPackageDto object by report package Id.
         /// </summary>
         /// <param name="reportPackageId">ReportPackageId</param>
-        /// <param name="incldingAttachmentFileData">Indicating attachments binary data is included or not</param>
-        /// <returns>ReportPackageDto Object</returns> 
+        /// <param name="isIncludeAssociatedElementData">Indicating attachments binary data is included or not</param>
+        /// <returns>ReportPackageDto Object</returns>
         ReportPackageDto GetReportPackage(int reportPackageId, bool isIncludeAssociatedElementData);
 
         /// <summary>
-        /// The file 'Copy of Record Data.xml' contains below:
-        /// 1.  The raw 'text' data including samples and results data, certification name and text, coments,
+        ///     The file 'Copy of Record Data.xml' contains below:
+        ///     1.  The raw 'text' data including samples and results data, certification name and text, comments,
         ///     List of files included in the .zip file,  and copy of record receipt details
-        /// 2.  Copy of Record receit, including UserProfile Details,  Organization Details, and ReportDetails
-        /// 3.  Copy of Record file Manifest: A list of earch file in the .zip      
+        ///     2.  Copy of Record receipt, including UserProfile Details,  Organization Details, and ReportDetails
+        ///     3.  Copy of Record file Manifest: A list of each file in the .zip
         /// </summary>
         /// <param name="reportPackageDto">The ReportPackageDto containing all detailed information </param>
         /// <returns>CopyOfRecordDataXmlFileDto</returns>
         CopyOfRecordDataXmlFileDto GetReportPackageCopyOfRecordDataXmlFile(ReportPackageDto reportPackageDto);
 
         /// <summary>
-        /// System generated formatted PDF containing(nearly all) data in the Copy of Record Data.xml, 
-        /// combined into a single formatted PDF. This is a human readable and printable report.
-        /// Format to match COR Standard Report Format.docx
+        ///     System generated formatted PDF containing(nearly all) data in the Copy of Record Data.xml,
+        ///     combined into a single formatted PDF. This is a human readable and printable report.
+        ///     Format to match COR Standard Report Format.docx
         /// </summary>
         /// <param name="reportPackageDto">The ReportPackageDto containing all detailed information </param>
         /// <returns>CopyOfRecordPdfFileDto</returns>
         CopyOfRecordPdfFileDto GetReportPackageCopyOfRecordPdfFile(ReportPackageDto reportPackageDto);
 
         /// <summary>
-        /// *WARNING: NO VALIDATION CHECK -- CASCADE DELETE*
-        /// Hard delete of row from tReportPackage table associated with passed in parameter.
-        /// Programatically cascade deletes rows in the following associated tables:
-        /// - tReportPackageElementCategory (via ReportPackageId)
-        /// - tReportPackageElementType (via ReportPackageElementCategoryId)
-        /// - tReportSample (via ReportPackageElementTypeId)
-        /// - tReportFile (via ReportPackageElementTypeId)
-        /// - tCopyofRecord (via ReportPackageId)
+        ///     *WARNING: NO VALIDATION CHECK -- CASCADE DELETE*
+        ///     Hard delete of row from tReportPackage table associated with passed in parameter.
+        ///     Programatically cascade deletes rows in the following associated tables:
+        ///     - tReportPackageElementCategory (via ReportPackageId)
+        ///     - tReportPackageElementType (via ReportPackageElementCategoryId)
+        ///     - tReportSample (via ReportPackageElementTypeId)
+        ///     - tReportFile (via ReportPackageElementTypeId)
+        ///     - tCopyofRecord (via ReportPackageId)
         /// </summary>
         /// <param name="reportPackageId">tReportPackage.ReportPackageId</param>
         void DeleteReportPackage(int reportPackageId);
 
         /// <summary>
-        /// To be called after a User selects a template and date range but before 
-        /// the User clicks the "Save Draft" button (no reportPackageDto to save yet)
+        ///     To be called after a User selects a template and date range but before
+        ///     the User clicks the "Save Draft" button (no reportPackageDto to save yet)
         /// </summary>
         /// <param name="reportPackageTemplateId"></param>
         /// <param name="startDateTimeLocal"></param>
         /// <param name="endDateTimeLocal"></param>
         /// <returns>The newly created tReportPackage.ReportPackageId</returns>
-
         int CreateDraft(int reportPackageTemplateId, DateTime startDateTimeLocal, DateTime endDateTimeLocal);
 
         /// <summary>
-        /// Cannot be used to CREATE, only to UPDATE. Use "CreateDraft" to create.
-        /// reportPackageDto.ReportPackageId must exist or exception thrown
+        ///     Cannot be used to CREATE, only to UPDATE. Use "CreateDraft" to create.
+        ///     reportPackageDto.ReportPackageId must exist or exception thrown
         /// </summary>
         /// <param name="reportPackageDto">Existing Report Package to update</param>
         /// <param name="isUseTransaction">If true, runs within transaction object</param>
@@ -74,30 +73,30 @@ namespace Linko.LinkoExchange.Services.Report
         int SaveReportPackage(ReportPackageDto reportPackageDto, bool isUseTransaction);
 
         /// <summary>
-        /// Performs validation to ensure only allowed state transitions are occur,
-        /// throw RuleViolationException otherwise. Does NOT enter any corrsponding values into the Report Package row.
+        ///     Performs validation to ensure only allowed state transitions are occur,
+        ///     throw RuleViolationException otherwise. Does NOT enter any corresponding values into the Report Package row.
         /// </summary>
+        /// <param name="reportPackageId"></param>
         /// <param name="reportStatus">Intended target state</param>
         /// <param name="isUseTransaction">If true, runs within transaction object</param>
-
         void UpdateStatus(int reportPackageId, ReportStatusName reportStatus, bool isUseTransaction);
 
         /// <summary>
-        /// Gets a collection of FileStoreDto's that are eligible to be added this Report Package
+        ///     Gets a collection of FileStoreDto's that are eligible to be added this Report Package
         /// </summary>
         /// <param name="reportPackageId">tReportPackage.ReportPackageId</param>
         /// <returns>Collection of FileStoreDto objects</returns>
         ICollection<FileStoreDto> GetFilesForSelection(int reportPackageId);
 
         /// <summary>
-        /// Gets a collection of SampleDto's that are eligible to be added this Report Package
+        ///     Gets a collection of SampleDto's that are eligible to be added this Report Package
         /// </summary>
         /// <param name="reportPackageId">tReportPackage.ReportPackageId</param>
         /// <returns>Collection of SampleDto objects</returns>
         ICollection<SampleDto> GetSamplesForSelection(int reportPackageId);
 
         /// <summary>
-        /// Gets Report Package information (without children element data) for displaying in a grid.
+        ///     Gets Report Package information (without children element data) for displaying in a grid.
         /// </summary>
         /// <param name="reportStatusName">Fetches report packages of this status only</param>
         /// <param name="isAuthorityViewing">True is User is Authority</param>
@@ -105,14 +104,14 @@ namespace Linko.LinkoExchange.Services.Report
         IEnumerable<ReportPackageDto> GetReportPackagesByStatusName(ReportStatusName reportStatusName, bool isAuthorityViewing);
 
         /// <summary>
-        /// Gets items to populate a dropdown list of reasons to repudiate a report package (for a specific Org Reg Program Id)
+        ///     Gets items to populate a dropdown list of reasons to repudiate a report package (for a specific Org Reg Program Id)
         /// </summary>
         /// <returns></returns>
         IEnumerable<RepudiationReasonDto> GetRepudiationReasons();
 
         /// <summary>
-        /// Performs various validation checks before putting a report package into "Repudiated" status.
-        /// Also logs action and emails stakeholders.
+        ///     Performs various validation checks before putting a report package into "Repudiated" status.
+        ///     Also logs action and emails stakeholders.
         /// </summary>
         /// <param name="reportPackageId">tReportPackage.ReportPackageId</param>
         /// <param name="repudiationReasonId">tRepudiationReason.RepudiationReasonId</param>
@@ -121,19 +120,19 @@ namespace Linko.LinkoExchange.Services.Report
         void RepudiateReport(int reportPackageId, int repudiationReasonId, string repudiationReasonName, string comments = null);
 
         /// <summary>
-        /// Meant to be called when user has reviewed a report submission. Updates the corresponding fields in the Report Package row.
+        ///     Meant to be called when user has reviewed a report submission. Updates the corresponding fields in the Report
+        ///     Package row.
         /// </summary>
         /// <param name="reportPackageId">tReportPackage.ReportPackageId</param>
         /// <param name="comments">Optional field</param>
         void ReviewSubmission(int reportPackageId, string comments = null);
 
         /// <summary>
-        /// Meant to be called when user has reviewed a report repudiation. Updates the corresponding fields in the Report Package row.
+        ///     Meant to be called when user has reviewed a report repudiation. Updates the corresponding fields in the Report
+        ///     Package row.
         /// </summary>
         /// <param name="reportPackageId">tReportPackage.ReportPackageId</param>
         /// <param name="comments">Required field</param>
         void ReviewRepudiation(int reportPackageId, string comments);
-
-
     }
 }
--- Linko.LinkoExchange.Web/Controllers/ReportPackageController.cs
+++ Linko.LinkoExchange.Web/Controllers/ReportPackageController.cs
@@ -11,9 +11,6 @@ using Linko.LinkoExchange.Services.Unit;
 
 namespace Linko.LinkoExchange.Web.Controllers
 {
-    /// <summary>
-    ///     This is a testing controller,  dev who implement this controller should replace it.
-    /// </summary>
     public class ReportPackageController:Controller
     {
         private readonly IHttpContextService _httpContextService;
@@ -55,88 +52,6 @@ namespace Linko.LinkoExchange.Web.Controllers
             var tempate = _reportTemplateService.GetReportPackageTemplate(reportPackageTemplateId:1);
 
             return Json(data:templatesList, behavior:JsonRequestBehavior.AllowGet);
-        }
-
-        /// <summary>
-        ///     Testing Update
-        /// </summary>
-        /// <returns></returns>
-        public JsonResult Update()
-        {
-            var templatesList = _reportTemplateService.GetReportPackageTemplates().ToList();
-
-            foreach (var rpt in templatesList)
-            {
-                rpt.Name += "_";
-                rpt.Description += "_";
-                rpt.ShowSampleResults = true;
-
-                _reportTemplateService.SaveReportPackageTemplate(rpt:rpt);
-            }
-
-            templatesList = _reportTemplateService.GetReportPackageTemplates().ToList();
-
-            return Json(data:templatesList, behavior:JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult AttachmentTypes()
-        {
-            var list = _reportTemplateService.GetAttachmentTypes();
-            return Json(data:list, behavior:JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult CertificationTypes()
-        {
-            var list = _reportTemplateService.GetCertificationTypes();
-            return Json(data:list, behavior:JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult GetIndustryPPT_ResultQualifiersIndustryCanUse()
-        {
-            var currentRegulatoryProgramId =
-                int.Parse(s:_httpContextService.GetClaimValue(claimType:CacheKey.OrganizationRegulatoryProgramId));
-
-            var settings = _settingService.GetSettingTemplateValue(settingType:SettingType.ResultQualifierValidValues);
-
-            return Json(data:settings, behavior:JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult update_ResultQualifiersIndustryCanUse()
-        {
-            var currentRegulatoryProgramId =
-                int.Parse(s:_httpContextService.GetClaimValue(claimType:CacheKey.OrganizationRegulatoryProgramId));
-
-            var settingDto = new SettingDto();
-            settingDto.TemplateName = SettingType.ResultQualifierValidValues;
-            settingDto.Value = "GreaterThan,LessThan";
-            settingDto.OrgTypeName = OrganizationTypeName.Authority;
-
-            _settingService.CreateOrUpdateProgramSetting(orgRegProgId:currentRegulatoryProgramId, settingDto:settingDto);
-
-            return Json(data:"Saving good", behavior:JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult GetIndustryPPT_CreateSampleNamesRule()
-        {
-            var currentRegulatoryProgramId =
-                int.Parse(s:_httpContextService.GetClaimValue(claimType:CacheKey.OrganizationRegulatoryProgramId));
-
-            var settings = _settingService.GetSettingTemplateValue(settingType:SettingType.SampleNameCreationRule);
-
-            return Json(data:settings, behavior:JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult GetUnits()
-        {
-            var units = _unitService.GetFlowUnits();
-            return Json(data:units, behavior:JsonRequestBehavior.AllowGet);
-        }
-
-        [Route(template:"submission/{reportPackageId:int}")]
-        public ViewResult Submission(int reportPackageId)
-        {
-            _reportPackageService.SignAndSubmitReportPackage(reportPackageId:reportPackageId);
-            return View();
         }
     }
 }
