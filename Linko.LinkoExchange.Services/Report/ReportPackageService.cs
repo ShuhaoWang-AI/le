ee70e897883e053cff50c23002f8c23d205bceb3
Shuhao Wang <shuhao.wang@aquaticinformatics.com>
2017-05-12 21:01:58 +0000

    Fix time format
    TFS Changeset 1057: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/1057 
    
    Workitems:
    [2507] Milestone 3 code
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=2507 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~2507

--- Linko.LinkoExchange.Services/Report/ReportPackageService.cs
+++ Linko.LinkoExchange.Services/Report/ReportPackageService.cs
@@ -598,7 +598,7 @@ namespace Linko.LinkoExchange.Services.Report
 
             var timeZoneNameAbbreviation = _timeZoneService.GetTimeZoneNameUsingSettingForThisOrg(reportPackage.OrganizationRegulatoryProgramId, reportPackage.SubmissionDateTimeLocal.Value, true);
             var submissionDateTime =
-                $"{reportPackage.SubmissionDateTimeLocal.Value.ToString("MMM dd, yyyy HH:tt tt ")}{timeZoneNameAbbreviation}";
+                $"{reportPackage.SubmissionDateTimeLocal.Value.ToString("MMM dd, yyyy HH:mm tt ")}{timeZoneNameAbbreviation}";
 
             emailContentReplacements.Add("submissionDateTime", submissionDateTime);
             emailContentReplacements.Add("corSignature", copyOfRecordDto.Signature);
