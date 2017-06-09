ad84b08c02bd050ac54277efd5b9376d9819aac3
Shuhao Wang <shuhao.wang@aquaticinformatics.com>
2017-06-09 17:49:35 +0000

    Fix confirm password validataion message is not showing
    TFS Changeset 1213: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/1213 
    
    Workitems:
    [3572] Coding: Fix confirm password error message is not showing
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=3572 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~3572

--- Linko.LinkoExchange.Web/ViewModels/Account/ChangePasswordViewModel.cs
+++ Linko.LinkoExchange.Web/ViewModels/Account/ChangePasswordViewModel.cs
@@ -48,15 +48,8 @@ namespace Linko.LinkoExchange.Web.ViewModels.Account
             RuleFor(x => x.Password.Length).GreaterThanOrEqualTo(valueToCompare: 8).WithMessage(errorMessage: "{PropertyName} must be more than 8 and less than or equal to 16 characters long.");
             RuleFor(x => x.Password).Matches(regexp).WithMessage(errorMessage: "{PropertyName} must be at least 8 characters, have at least one digit ('0'-'9'), and have at least one lowercase ('a'-'z') and one uppercase ('A'-'Z') letter. ");
 
-            When(x => x.ShowConfirmPassword, () =>
-                                           {
-                                               RuleFor(x => x.ConfirmPassword)
-                                                  .NotEmpty()
-                                                  .WithMessage(errorMessage: "{PropertyName} is required.");
-                                               RuleFor(x => x.ConfirmPassword)
-                                                  .Equal(x => x.Password)
-                                                  .WithMessage(errorMessage: "The Password and Confirm Password do not match.");
-                                          });
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage(errorMessage: "{PropertyName} is required.");
+            RuleFor(x => x.ConfirmPassword).Equal(x => x.Password).WithMessage(errorMessage: "The Password and Confirm Password do not match.");
         } 
     }
 }
