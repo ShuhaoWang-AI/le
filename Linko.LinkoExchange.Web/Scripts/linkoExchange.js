a820f1e2a3065284043030e01aedb4a8bae298bc
Rajeeb Saha <rajeeb.saha@aquaticinformatics.com>
2017-06-13 05:07:33 +0000

    Fixed the max length issue for Attachment Description, Report Package comment, Repudiated comment, submission and repudiated review comments. Forcing user can not enter characters more than max length. If enter then will trim from end. Need to add custom length calculation for textarea as different system treat the newline character differently and end up allowing more characters and server side throws error.
    Fixed issue with "Certification and Comments section does not maintain line breaks".
    
    TFS Changeset 1227: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/1227 
    
    Workitems:
    [3641] Coding - Bug 3592:RP - Internal error message when Comment exceeds 500 characters
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=3641 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~3641
    [3642] Coding - Bug 3591:Submitted Report Review - Error when Comments are greater than 500 characters
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=3642 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~3642
    [3643] Coding - Bug 3590:Repudiated Report Review - Error when Comments are greater than 500 characters
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=3643 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~3643
    [3644] Coding - Bug 3106:Attachment Details - Internal error when description exceeds 500 characters
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=3644 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~3644
    [3651] Coding - Bug 3576:RP - Certification and Comments section does not maintain line breaks
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=3651 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~3651

--- Linko.LinkoExchange.Web/Scripts/linkoExchange.js
+++ Linko.LinkoExchange.Web/Scripts/linkoExchange.js
@@ -1,106 +1,122 @@
-﻿doAjaxAction = function (grid, postUrl, returnUrl, noSelectionMessage)
+﻿doAjaxAction = function(grid, postUrl, returnUrl, noSelectionMessage)
 {
     var selection = [];
 
-    grid.select().each(
-        function ()
-        {
-            var dataItem = grid.dataItem($(this));
-            selection.push(dataItem);
-        }
-    );
+    grid.select()
+        .each(
+            function()
+            {
+                var dataItem = grid.dataItem($(this));
+                selection.push(dataItem);
+            }
+        );
 
     if (selection.length > 0)
     {
         $.ajax({
-            type: "POST",
-            url: postUrl,
-            data: JSON.stringify({ returnUrl: returnUrl, items: selection }),
-            dataType: "JSON",
-            contentType: "application/json; charset=utf-8",
-            success: function (returnData)
+            type: "POST"
+            , url: postUrl
+            , data: JSON.stringify({ returnUrl: returnUrl, items: selection })
+            , dataType: "JSON"
+            , contentType: "application/json; charset=utf-8"
+            , success: function(returnData)
             {
                 if ($.trim(returnData.message).length > 0)
                 {
-                    $('body').append('<div class="popup" data-popup="popup-1">' +
-                                        '<div class="popup-inner">' +
-                                            '<h4>' + returnData.message + '</h4>' +
-                                            '<a class="popup-close" data-popup-close="popup-1" href="#">x</a>' +
-                                        '</div>' +
-                                    '</div>');
+                    $("body")
+                        .append('<div class="popup" data-popup="popup-1">'
+                            + '<div class="popup-inner">'
+                            + "<h4>"
+                            + returnData.message
+                            + "</h4>"
+                            + '<a class="popup-close" data-popup-close="popup-1" href="#">x</a>'
+                            + "</div>"
+                            + "</div>");
 
                     $('[data-popup="popup-1"]').fadeIn(350);
                     $('[data-popup="popup-1"]').delay(2500);
                     $('[data-popup="popup-1"]').fadeOut(350);
 
-                    $('[data-popup-close]').on('click', function (e)
-                    {
-                        $('[data-popup="popup-1"]').fadeOut(350);
-                    });
+                    $("[data-popup-close]")
+                        .on("click"
+                            , function(e)
+                            {
+                                $('[data-popup="popup-1"]').fadeOut(350);
+                            });
                 }
 
-                setTimeout(function ()
-                {
-                    if (returnData.redirect)
+                setTimeout(function()
                     {
-                        //$(location).attr("href", returnData.newurl);
-                        window.location.replace(returnData.newurl);
+                        if (returnData.redirect)
+                        {
+                            //$(location).attr("href", returnData.newurl);
+                            window.location.replace(returnData.newurl);
+                        }
                     }
-                }, 500);
+                    , 500);
             }
         });
     }
     else
     {
-        $('body').append('<div class="popup" data-popup="popup-1">' +
-                                        '<div class="popup-inner">' +
-                                            '<h4>' + noSelectionMessage + '</h4>' +
-                                            '<a class="popup-close" data-popup-close="popup-1" href="#">x</a>' +
-                                        '</div>' +
-                                    '</div>');
+        $("body")
+            .append('<div class="popup" data-popup="popup-1">'
+                + '<div class="popup-inner">'
+                + "<h4>"
+                + noSelectionMessage
+                + "</h4>"
+                + '<a class="popup-close" data-popup-close="popup-1" href="#">x</a>'
+                + "</div>"
+                + "</div>");
 
         $('[data-popup="popup-1"]').fadeIn(350);
         $('[data-popup="popup-1"]').fadeOut(3000);
 
-        $('[data-popup-close]').on('click', function (e)
-        {
-            $('[data-popup="popup-1"]').fadeOut(350);
-        });
+        $("[data-popup-close]")
+            .on("click"
+                , function(e)
+                {
+                    $('[data-popup="popup-1"]').fadeOut(350);
+                });
     }
 };
 
-$(document).on('ajaxError', function (event, jqXHR)
-{
-    var json_response = jqXHR.getResponseHeader("X-Responded-JSON");
-
-    if (json_response)
-    {
-        var json_response_obj = JSON.parse(json_response);
-
-        if (json_response_obj.status == 401)
+$(document)
+    .on("ajaxError"
+        , function(event, jqXHR)
         {
-            $(location).attr("href", json_response_obj.headers.location); //login URL
-            return;
-        }
-    }
-});
+            var json_response = jqXHR.getResponseHeader("X-Responded-JSON");
 
-error_handler = function (e)
+            if (json_response)
+            {
+                var json_response_obj = JSON.parse(json_response);
+
+                if (json_response_obj.status == 401)
+                {
+                    $(location).attr("href", json_response_obj.headers.location); //login URL
+                    return;
+                }
+            }
+        });
+
+error_handler = function(e)
 {
     if (e.errors)
     {
         var message = "There are some errors:\n";
         // Create a message containing all errors.
-        $.each(e.errors, function (key, value)
-        {
-            if ('errors' in value)
+        $.each(e.errors
+            , function(key, value)
             {
-                $.each(value.errors, function ()
+                if ("errors" in value)
                 {
-                    message += this + "\n";
-                });
-            }
-        });
+                    $.each(value.errors
+                        , function()
+                        {
+                            message += this + "\n";
+                        });
+                }
+            });
 
         // Display the message.
         showPopupMessage(message);
@@ -109,37 +125,68 @@ error_handler = function (e)
         var grid = $("[name^='grid']").data("kendoGrid");
         grid.cancelChanges();
     }
-}
-
-showPopupMessage = function (message)
+};
+showPopupMessage = function(message)
 {
-
-    $('body').append('<div class="popup" data-popup="popup-1">' +
-                        '<div class="popup-inner">' +
-                            '<h4>' + message + '</h4>' +
-                            '<a class="popup-close" data-popup-close="popup-1" href="#">x</a>' +
-                        '</div>' +
-                    '</div>');
+    $("body")
+        .append('<div class="popup" data-popup="popup-1">'
+            + '<div class="popup-inner">'
+            + "<h4>"
+            + message
+            + "</h4>"
+            + '<a class="popup-close" data-popup-close="popup-1" href="#">x</a>'
+            + "</div>"
+            + "</div>");
 
     $('[data-popup="popup-1"]').fadeIn(350);
 
-    $('[data-popup-close]').on('click', function (e)
-    {
-        $('[data-popup="popup-1"]').fadeOut(350);
-    });
-}
-
+    $("[data-popup-close]")
+        .on("click"
+            , function(e)
+            {
+                $('[data-popup="popup-1"]').fadeOut(350);
+            });
+};
 $(document)
     .ready(function()
     {
         $("#LinkoExchangeForm")
             .submit(function(e)
             {
-                $("input[type='submit']").prop('disabled', true);
+                $("input[type='submit']").prop("disabled", true);
                 //setTimeout(function()
                 //    {
                 //        $("input[type='submit']").prop('disabled', false);
                 //    }
                 //    , 2000);
             });
+
+        // Get all textareas that have a "maxlength" property.
+        $("textarea[maxlength]")
+            .on("keyup blur"
+                , function()
+                {
+                    //Counts all the newline characters (\r = return for macs, \r\n for Windows, \n for Linux/unix)
+                    var newLineCharacterRegexMatch = /\r?\n|\r/g;
+
+                    // Store the maxlength and value of the field.
+                    var maxlength = $(this).attr("maxlength");
+                    var val = $(this).val();
+
+                    //count newline characters
+                    var regexResult = val.match(newLineCharacterRegexMatch);
+                    var newLineCount = regexResult ? regexResult.length : 0;
+
+                    //replace newline characters with nothing
+                    var replacedValue = val.replace(newLineCharacterRegexMatch, "");
+
+                    //return the length of text without newline characters + doubled newline character count
+                    var length = replacedValue.length + (newLineCount * 2);
+                    
+                    // Trim the field if it has content over the maxlength.
+                    if (length > maxlength)
+                    {
+                        $(this).val(val.slice(0, val.length - (length - maxlength)));
+                    }
+                });
     });
