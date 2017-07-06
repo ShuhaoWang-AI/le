0a0cb5d0a7f1baa5251fa445bffc3d9465f44b3b
Markus Jeon <WATERTRAX\mjeon>
2017-07-06 18:21:48 +0000

    Updated the error message
    TFS Changeset 1440: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/1440 
    
    Workitems:
    [4508] Coding: Change Password - Password requirements are shown below the fields
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=4508 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~4508

--- Linko.LinkoExchange.Web/ViewModels/Account/ChangePasswordViewModel.cs
+++ Linko.LinkoExchange.Web/ViewModels/Account/ChangePasswordViewModel.cs
@@ -56,9 +56,9 @@ namespace Linko.LinkoExchange.Web.ViewModels.Account
             var regexp = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d$@$#!%^&#'*""?,;:&.<>_\-\[\]\{\}\(\)\+\=~\|\\\?/]{8,16}$";  
 
             RuleFor(x => x.Password).NotEmpty().WithMessage(errorMessage: "{PropertyName} is required.");
-            RuleFor(x => x.Password.Length).LessThanOrEqualTo(valueToCompare: 16).WithMessage(errorMessage: "{PropertyName} must be more than 8 and less than or equal to 16 characters long.");
-            RuleFor(x => x.Password.Length).GreaterThanOrEqualTo(valueToCompare: 8).WithMessage(errorMessage: "{PropertyName} must be more than 8 and less than or equal to 16 characters long.");
-            RuleFor(x => x.Password).Matches(regexp).WithMessage(errorMessage: "{PropertyName} must be at least 8 characters, have at least one digit ('0'-'9'), and have at least one lowercase ('a'-'z') and one uppercase ('A'-'Z') letter. ");
+            RuleFor(x => x.Password.Length).LessThanOrEqualTo(valueToCompare: 16).WithMessage(errorMessage: "{PropertyName} does not meet the password requirements.");
+            RuleFor(x => x.Password.Length).GreaterThanOrEqualTo(valueToCompare: 8).WithMessage(errorMessage: "{PropertyName} does not meet the password requirements.");
+            RuleFor(x => x.Password).Matches(regexp).WithMessage(errorMessage: "{PropertyName} does not meet the password requirements.");
 
             RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage(errorMessage: "{PropertyName} is required.");
             RuleFor(x => x.ConfirmPassword).Equal(x => x.Password).WithMessage(errorMessage: "The Password and Confirm Password do not match.");
