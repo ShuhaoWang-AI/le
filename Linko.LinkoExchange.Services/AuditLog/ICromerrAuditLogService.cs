eb7774337dc5eb4126016c62bc9df1feadc0cfff
Markus Jeon <WATERTRAX\mjeon>
2017-07-25 21:30:06 +0000

    Comments added to interface class method in ICromerrAuditLogService
    TFS Changeset 1519: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/1519 
    
    Workitems:
    [4862] Code refactoring, comments, and general cleanup
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=4862 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~4862

--- Linko.LinkoExchange.Services/AuditLog/ICromerrAuditLogService.cs
+++ Linko.LinkoExchange.Services/AuditLog/ICromerrAuditLogService.cs
@@ -17,14 +17,48 @@ namespace Linko.LinkoExchange.Services.AuditLog
         /// <returns></returns>
         Task Log(CromerrEvent eventType, CromerrAuditLogEntryDto dto, IDictionary<string, string> contentReplacements);
 
+        /// <summary>
+        /// Can be used in multiple scenarios that all share the same content replacement items in the
+        /// comment field:
+        /// - "organizationName"
+        /// - "firstName"
+        /// - "lastName"
+        /// - "userName"
+        /// - "emailAddress"
+        /// </summary>
+        /// <param name="eventType"></param>
+        /// <param name="orgRegProgram">Target Org Reg Program of the event</param>
+        /// <param name="user">Target user of the event</param>
+        /// <returns></returns>
         Task SimpleLog(CromerrEvent eventType, OrganizationRegulatoryProgramDto orgRegProgram, UserDto user);
 
+        /// <summary>
+        /// Returns Cromerr audit log entries that match search parameters
+        /// </summary>
+        /// <param name="organizationRegulatoryProgramId">The currently logged in the user's org reg program</param>
+        /// <param name="page">Which page being requested</param>
+        /// <param name="pageSize">Rows per page</param>
+        /// <param name="sortColumn">Which column to sort by</param>
+        /// <param name="isSortAscending">Sort direction</param>
+        /// <param name="dateRangeStart">For date range search</param>
+        /// <param name="dateRangeEnd">For date range search</param>
+        /// <param name="dateToExclude">Exclusion date search</param>
+        /// <param name="eventCategoryContains">Search parameter</param>
+        /// <param name="eventTypeContains">Search parameter</param>
+        /// <param name="emailAddressContains">Search parameter</param>
+        /// <param name="totalCount">Integer passed out for kendo grid requirement</param>
+        /// <returns></returns>
         ICollection<CromerrAuditLogEntryDto> GetCromerrAuditLogEntries(int organizationRegulatoryProgramId,
             int page, int pageSize, string sortColumn, bool isSortAscending,
             DateTime? dateRangeStart, DateTime? dateRangeEnd, DateTime? dateToExclude, 
             string eventCategoryContains, string eventTypeContains, string emailAddressContains,
             out int totalCount);
 
+        /// <summary>
+        /// Returns a single Cromerr log entry
+        /// </summary>
+        /// <param name="cromerrAuditLogId">tCromerrAuditLog.CromerrAuditLogId</param>
+        /// <returns></returns>
         CromerrAuditLogEntryDto GetCromerrAuditLogEntry(int cromerrAuditLogId);
     }
 }
