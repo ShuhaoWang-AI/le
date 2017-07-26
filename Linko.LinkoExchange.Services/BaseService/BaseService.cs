bd256326af4f01241706174c30b14b9354a70b57
Markus Jeon <WATERTRAX\mjeon>
2017-07-26 21:50:27 +0000

    Added comments missing from BaseService abstract class method(s).
    TFS Changeset 1527: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/1527 
    
    Workitems:
    [4862] Code refactoring, comments, and general cleanup
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=4862 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~4862

--- Linko.LinkoExchange.Services/BaseService/BaseService.cs
+++ Linko.LinkoExchange.Services/BaseService/BaseService.cs
@@ -2,8 +2,20 @@
 
 namespace Linko.LinkoExchange.Services
 {
+    /// <summary>
+    /// All service classes must inherit from this base class.
+    /// Currently only used to implement function-level authorization. 
+    /// </summary>
     abstract public class BaseService
     {
+        /// <summary>
+        /// Returns a boolean value indicating authorization to execute a method.
+        /// This should be the first thing called upon entering a method.
+        /// Returning false should typically trigger an UnauthorizedAccess exception
+        /// </summary>
+        /// <param name="apiName">Method name (automatically detected without explicit parameter)</param>
+        /// <param name="id">Method specific "target" id of the object which the user is acting upon</param>
+        /// <returns></returns>
         abstract public bool CanUserExecuteApi([CallerMemberName] string apiName = "", params int[] id);
     }
 }
