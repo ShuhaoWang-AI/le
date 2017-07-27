0757b60c95c39968d53c201f55510b960c981097
Markus Jeon <WATERTRAX\mjeon>
2017-07-27 18:43:23 +0000

    Added missing comments from Http Context Service interface methods.
    TFS Changeset 1531: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/1531 
    
    Workitems:
    [4862] Code refactoring, comments, and general cleanup
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=4862 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~4862

--- Linko.LinkoExchange.Services/HttpContext/IHttpContextService.cs
+++ Linko.LinkoExchange.Services/HttpContext/IHttpContextService.cs
@@ -2,14 +2,58 @@
 {
     public interface IHttpContextService
     {
+        /// <summary>
+        /// Returns the current httpcontext 
+        /// </summary>
+        /// <returns></returns>
         System.Web.HttpContext Current();
+
+        /// <summary>
+        /// Returns the fully qualified URL of the current request
+        /// </summary>
+        /// <returns></returns>
         string GetRequestBaseUrl();
+
+        /// <summary>
+        /// Returns the first identity claim associated with the claim type or empty string
+        /// if none exists.
+        /// </summary>
+        /// <param name="claimType"></param>
+        /// <returns></returns>
         string GetClaimValue(string claimType);
+
+        /// <summary>
+        /// Returns an object from the session cache if it exists. Null otherwise.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         object GetSessionValue(string key);
+
+        /// <summary>
+        /// Puts an object into the session cache
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
         void SetSessionValue(string key, object value);
 
+        /// <summary>
+        /// Returns the tUserProfile.UserProfileId associated with the logged in user's identidy.
+        /// If no such user exists, the value returned is -1.
+        /// </summary>
+        /// <returns></returns>
         int CurrentUserProfileId();
+
+        /// <summary>
+        /// Returns client's IP address
+        /// </summary>
+        /// <returns></returns>
         string CurrentUserIPAddress();
+
+        /// <summary>
+        /// Attempts to find a host name associated with the client's IP address using
+        /// reverse DNS lookup. Returns null or empty string otherwise.
+        /// </summary>
+        /// <returns></returns>
         string CurrentUserHostName();
     }
 }
