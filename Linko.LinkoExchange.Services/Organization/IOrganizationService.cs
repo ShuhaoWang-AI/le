6617883675c66a27ad3d4258d470b2189209bf80
Markus Jeon <WATERTRAX\mjeon>
2017-07-27 22:20:24 +0000

    Added missing comments for various organization service interface methods.
    TFS Changeset 1534: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/1534 
    
    Workitems:
    [4564] Coding - Code Clean Up & Review
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=4564 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~4564

--- Linko.LinkoExchange.Services/Organization/IOrganizationService.cs
+++ Linko.LinkoExchange.Services/Organization/IOrganizationService.cs
@@ -37,18 +37,61 @@ namespace Linko.LinkoExchange.Services.Organization
         /// Otherwise throw exception
         EnableOrganizationResultDto UpdateEnableDisableFlag(int orgRegProgId, bool isEnabled, bool isAuthorizationRequired = false);
 
+        /// <summary>
+        /// Returns the org reg program object associated with the passed in id
+        /// </summary>
+        /// <param name="orgRegProgId">tOrganizationRegulatoryProgramId.OrganizationRegulatoryProgramId</param>
+        /// <param name="isAuthorizationRequired"></param>
+        /// <returns></returns>
         OrganizationRegulatoryProgramDto GetOrganizationRegulatoryProgram(int orgRegProgId, bool isAuthorizationRequired = false);
 
+        /// <summary>
+        /// Returns a collection of industry org reg programs that have as their regulator the passed org reg program.
+        /// Optionally filters the collection using a string "contains" searcg against the following fields:
+        ///     - Organization name
+        ///     - Organization street address lines
+        ///     - Organization city
+        ///     - Organization postal/zip code
+        /// </summary>
+        /// <param name="orgRegProgId"></param>
+        /// <param name="searchString"></param>
+        /// <returns></returns>
         List<OrganizationRegulatoryProgramDto> GetChildOrganizationRegulatoryPrograms(int orgRegProgId, string searchString = null);
 
+        /// <summary>
+        /// Returns the difference between the maximum allowable number of users and current user count.
+        /// </summary>
+        /// <param name="orgRegProgramId"></param>
+        /// <returns></returns>
         int GetRemainingUserLicenseCount(int orgRegProgramId);
 
+        /// <summary>
+        /// Returns the difference between the maximum allowable child industries and the current industry count.
+        /// </summary>
+        /// <param name="orgRegProgramId"></param>
+        /// <returns></returns>
         int GetRemainingIndustryLicenseCount(int orgRegProgramId);
 
+        /// <summary>
+        /// Returns the number of org reg program users within the passed in org reg program.
+        /// </summary>
+        /// <param name="orgRegProgramId"></param>
+        /// <returns></returns>
         int GetCurrentUserLicenseCount(int orgRegProgramId);
 
+        /// <summary>
+        /// Returns the number of org reg programs that have the passed in org reg program as their regulator.
+        /// </summary>
+        /// <param name="orgRegProgramId"></param>
+        /// <returns></returns>
         int GetCurrentIndustryLicenseCount(int orgRegProgramId);
 
+        /// <summary>
+        /// Gets the regulator of the passed in org reg program. In the case where there is no regulator,
+        /// the passed in org reg program itself is returned.
+        /// </summary>
+        /// <param name="orgRegProgramId"></param>
+        /// <returns></returns>
         OrganizationRegulatoryProgramDto GetAuthority(int orgRegProgramId);
     }
 }
