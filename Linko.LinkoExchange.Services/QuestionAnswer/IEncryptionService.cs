9ab4d235bcdc54f960168856891931c497945895
Markus Jeon <WATERTRAX\mjeon>
2017-07-28 16:20:36 +0000

    Added missing comments to encryption service interface methods.
    TFS Changeset 1538: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/1538 
    
    Workitems:
    [4564] Coding - Code Clean Up & Review
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=4564 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~4564

--- Linko.LinkoExchange.Services/QuestionAnswer/IEncryptionService.cs
+++ Linko.LinkoExchange.Services/QuestionAnswer/IEncryptionService.cs
@@ -3,7 +3,18 @@ namespace Linko.LinkoExchange.Services.QuestionAnswer
 {
     public interface IEncryptionService
     {
+        /// <summary>
+        /// Converts a human readable string to an encrypted string.
+        /// </summary>
+        /// <param name="readableString"></param>
+        /// <returns></returns>
         string EncryptString(string readableString);
+        
+        /// <summary>
+        /// Converts an encrypted string to a human readable string.
+        /// </summary>
+        /// <param name="encryptedString"></param>
+        /// <returns></returns>
         string DecryptString(string encryptedString);
     }
 }
