ba482acb33e7ca33bba5a2b68df243efab972606
Markus Jeon <WATERTRAX\mjeon>
2017-07-28 15:17:41 +0000

    Performance optimization: moved logic to read encryption password from config file from contructor to methods that require it.
    TFS Changeset 1537: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/1537 
    
    Workitems:
    [4564] Coding - Code Clean Up & Review
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=4564 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~4564

--- Linko.LinkoExchange.Services/QuestionAnswer/EncryptionService.cs
+++ Linko.LinkoExchange.Services/QuestionAnswer/EncryptionService.cs
@@ -4,21 +4,20 @@ namespace Linko.LinkoExchange.Services.QuestionAnswer
 {
     public class EncryptionService : IEncryptionService
     {
-        private string _encryptionPassword;
-
         public EncryptionService()
         {
-            _encryptionPassword = ConfigurationManager.AppSettings["EncryptionPassword"];
         }
 
         public string EncryptString(string readableString)
         {
-            return StringCipher.Encrypt(readableString, _encryptionPassword);
+            var encryptionPassword = ConfigurationManager.AppSettings["EncryptionPassword"];
+            return StringCipher.Encrypt(readableString, encryptionPassword);
         }
 
         public string DecryptString(string encryptedString)
         {
-            return StringCipher.Decrypt(encryptedString, _encryptionPassword);
+            var encryptionPassword = ConfigurationManager.AppSettings["EncryptionPassword"];
+            return StringCipher.Decrypt(encryptedString, encryptionPassword);
         }
 
     }
