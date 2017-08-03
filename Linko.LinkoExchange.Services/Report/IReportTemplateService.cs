fd363cbe50e90850fb551cb55c4971cff9302e5a
Markus Jeon <WATERTRAX\mjeon>
2017-08-03 19:00:15 +0000

    Added missing comments to report template service interface methods.
    TFS Changeset 1573: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/1573 
    
    Workitems:
    [4564] Coding - Code Clean Up & Review
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=4564 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~4564

--- Linko.LinkoExchange.Services/Report/IReportTemplateService.cs
+++ Linko.LinkoExchange.Services/Report/IReportTemplateService.cs
@@ -11,8 +11,21 @@ namespace Linko.LinkoExchange.Services.Report
         IEnumerable<ReportPackageTemplateDto> GetReportPackageTemplates(bool isForCreatingDraft = false, bool includeChildObjects = true);
         int SaveReportPackageTemplate(ReportPackageTemplateDto rpt);
 
+        /// <summary>
+        /// Returns a collection of CTS event types associated with the authority of the current regulatory program.
+        /// Can optionally filter for event types in the "sample" category.
+        /// </summary>
+        /// <param name="isForSample"></param>
+        /// <returns></returns>
         IEnumerable<CtsEventTypeDto> GetCtsEventTypes(bool isForSample);
+
+        /// <summary>
+        /// Gets a specific CTS event type with its date/time fields localized to the current regulatory program's authority.
+        /// </summary>
+        /// <param name="ctsEventTypeId"></param>
+        /// <returns></returns>
         CtsEventTypeDto GetCtsEventType(int ctsEventTypeId);
+
         IEnumerable<ReportElementTypeDto> GetCertificationTypes();
         IEnumerable<ReportElementTypeDto> GetAttachmentTypes();
         IEnumerable<ReportElementTypeDto> GetSampleAndResultTypes();
