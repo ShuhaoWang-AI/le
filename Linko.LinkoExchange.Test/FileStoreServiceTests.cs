1574be5cebde705fcb72e1de3635ef0947e2575a
Markus Jeon <WATERTRAX\mjeon>
2017-08-10 21:23:38 +0000

    New unit test for CanUserExecuteApi GetFileStoreById AsAuthority (authorized) - file store is included in at least one report package that is submitted or repudiated.
    TFS Changeset 1598: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/1598 
    
    Workitems:
    [4564] Coding - Code Clean Up & Review
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=4564 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~4564

--- Linko.LinkoExchange.Test/FileStoreServiceTests.cs
+++ Linko.LinkoExchange.Test/FileStoreServiceTests.cs
@@ -9,12 +9,16 @@ using Linko.LinkoExchange.Services.TimeZone;
 using Linko.LinkoExchange.Services.FileStore;
 using Linko.LinkoExchange.Services.Cache;
 using Linko.LinkoExchange.Services.HttpContext;
+using Linko.LinkoExchange.Services.Dto;
+using System;
+using Linko.LinkoExchange.Core.Domain;
 
 namespace Linko.LinkoExchange.Test
 {
     [TestClass]
     public class FileStoreServiceTests
     {
+        LinkoExchangeContext _dbContext;
         FileStoreService _fileStoreService;
         Mock<ITimeZoneService> _timeZoneService = new Mock<ITimeZoneService>();
         Mock<ILogger> _logger = new Mock<ILogger>();
@@ -29,18 +33,18 @@ namespace Linko.LinkoExchange.Test
         public void Initialize()
         {
             var connectionString = ConfigurationManager.ConnectionStrings["LinkoExchangeContext"].ConnectionString;
-            var connection = new LinkoExchangeContext(connectionString);
+            _dbContext = new LinkoExchangeContext(connectionString);
             _httpContext = new Mock<IHttpContextService>();
             _logger = new Mock<ILogger>();
             _timeZoneService = new Mock<ITimeZoneService>();
-            var actualSettingService = new SettingService(connection, _logger.Object, new MapHelper(), new Mock<IRequestCache>().Object, new Mock<IGlobalSettings>().Object);
-            var actualTimeZoneService = new TimeZoneService(connection, actualSettingService, new MapHelper(), new Mock<IApplicationCache>().Object);
+            var actualSettingService = new SettingService(_dbContext, _logger.Object, new MapHelper(), new Mock<IRequestCache>().Object, new Mock<IGlobalSettings>().Object);
+            var actualTimeZoneService = new TimeZoneService(_dbContext, actualSettingService, new MapHelper(), new Mock<IApplicationCache>().Object);
 
             _httpContext.Setup(s => s.GetClaimValue(CacheKey.OrganizationRegulatoryProgramId)).Returns("1");
             _httpContext.Setup(s => s.GetClaimValue(CacheKey.PortalName)).Returns("authority");
 
             _fileStoreService = new FileStoreService(
-                connection,
+                _dbContext,
                 new MapHelper(),
                 _logger.Object,
                 _httpContext.Object,
@@ -130,6 +134,39 @@ namespace Linko.LinkoExchange.Test
             var isAuthorized = _fileStoreService.CanUserExecuteApi("GetFileStoreById", fileStoreId);
 
             Assert.IsFalse(isAuthorized);
+        }
+
+        [TestMethod]
+        public void CanUserExecuteApi_GetFileStoreById_AsAuthority_True_Test()
+        {
+            //Setup mocks
+            _httpContext.Setup(s => s.GetClaimValue(CacheKey.PortalName)).Returns("authority");
+            _httpContext.Setup(s => s.GetClaimValue(CacheKey.OrganizationRegulatoryProgramId)).Returns("1");
+            _httpContext.Setup(s => s.GetClaimValue(CacheKey.UserProfileId)).Returns("1");
+            
+            //Create file object in the database
+            var fileStoreDto = new FileStoreDto()
+            {
+                Name = "Test File Store Name",
+                OriginalFileName = "test_filestore.docx",
+                SizeByte = 1.1,
+                FileTypeId = 1,
+                ReportElementTypeId = 1,
+                ReportElementTypeName = "RPET Name",
+                OrganizationRegulatoryProgramId = 1,
+                UploadDateTimeLocal = DateTime.Now,
+                UploaderUserId = 1,
+                Data = new byte[1]
+            };
+
+            var fileStoreId = _fileStoreService.CreateFileStore(fileStoreDto);
+
+            _dbContext.ReportFiles.Add(new ReportFile() { ReportPackageElementTypeId = 1, FileStoreId = fileStoreId });
+            _dbContext.SaveChanges();
+
+            var isAuthorized = _fileStoreService.CanUserExecuteApi("GetFileStoreById", fileStoreId);
+
+            Assert.IsTrue(isAuthorized);
         }
 
     }
