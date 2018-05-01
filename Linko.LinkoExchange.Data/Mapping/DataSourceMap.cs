5226fecc77766d8c19804d1aa14341f1c2648ba3
Alan Liu <alan.liu@aquaticinformatics.com>
2018-05-01 21:15:51 +0000

    Task #8239 Coding - Added  entity data mapper for new domain object DataSource
    TFS Changeset 2180: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/2180 
    
    Workitems:
    [8239] Coding- User Story 8186:Industry Portal - Add, Edit, Delete Data Source
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=8239 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~8239

---
+++ Linko.LinkoExchange.Data/Mapping/DataSourceMap.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Linko.LinkoExchange.Core.Domain;
+
+namespace Linko.LinkoExchange.Data.Mapping
+{
+    public class DataSourceMap : EntityTypeConfiguration<DataSource>
+    {
+        #region constructors and destructor
+
+        public DataSourceMap()
+        {
+            ToTable(tableName: "tDataSource");
+
+            HasKey(x => x.DataSourceId);
+
+            Property(x => x.Name).IsRequired().HasMaxLength(value:100);
+
+            Property(x => x.Description).IsOptional().HasMaxLength(value:500);
+
+            Property(x => x.OrganizationRegulatoryProgramId).IsRequired();
+
+            Property(x => x.CreationDateTimeUtc).IsRequired();
+
+            Property(x => x.LastModificationDateTimeUtc).IsOptional();
+
+            Property(x => x.LastModifierUserId).IsOptional();
+        }
+
+        #endregion
+    }
+}
