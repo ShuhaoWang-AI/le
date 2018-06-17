72116ccd35f8998ee156f159da23587bde680d86
Alan Liu <alan.liu@aquaticinformatics.com>
2018-06-17 06:10:06 +0000

    Tweaked jquery selector to our switch by using name contains keyword 'onOffSwitch' instead of name equal to 'onOffSwitchSearchControl'
    TFS Changeset 2313: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/2313 
    
    Workitems:
    [8524] Data Translations rework
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=8524 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~8524

--- Linko.LinkoExchange.Web/Scripts/AdminLTE/init.js
+++ Linko.LinkoExchange.Web/Scripts/AdminLTE/init.js
@@ -4,8 +4,7 @@
     $(".datepicker").datepicker({
         autoclose: true
     });
-
-    $('input[type="checkbox"], input[type="radio"]').not('[name="onOffSwitchSearchControl"]').iCheck({
+    $('input[type="checkbox"], input[type="radio"]').not('[name*="onOffSwitch"]').iCheck({
         checkboxClass: "icheckbox_minimal-blue",
         radioClass: "iradio_minimal-blue"
     });
