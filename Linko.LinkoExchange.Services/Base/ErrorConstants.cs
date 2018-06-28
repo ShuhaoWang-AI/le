1cacf6e02c3def2af83681932cfc5677e5290bad
Shuhao Wang <shuhao.wang@aquaticinformatics.com>
2018-06-28 18:57:48 +0000

    Change error message
    TFS Changeset 2397: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/LinkoApplications/_versionControl/changeset/2397 
    
    Workitems:
    [8686] Coding: show validation error message when there is ND in the flow row
    TFS: http://wtrxtfs2013.watertrax.local:8080/tfs/Linko/WorkItemTracking/WorkItem.aspx?artifactMoniker=8686 
    Jira: https://aquaticinformatics.atlassian.net/issues/?jql=project%20%3D%20LINKO%20and%20tfsissueid~8686

--- Linko.LinkoExchange.Services/Base/ErrorConstants.cs
+++ Linko.LinkoExchange.Services/Base/ErrorConstants.cs
@@ -58,11 +58,11 @@ namespace Linko.LinkoExchange.Services.Base
                 public const string FieldValueIsRequired = "{0} is required and is missing.";
 
                 public const string DuplicateParametersInSameSample = "Duplicate parameters exist for the same sample.";
-                public const string FlowUnitIsInvalidOnMassLoadingCalculation = "Invalid Flow unit for Mass Loadings calculations.Chosen unit must be {0}.";
+                public const string FlowUnitIsInvalidOnMassLoadingCalculation = "Invalid Flow unit for Mass Loadings calculations. Chosen unit must be {0}.";
                 public const string FlowUnitIsUnSpecified = "Missing flow units for Mass Loading calculations.";
                 public const string FlowValueIsInvalid = "Missing flow value for Mass Loading calculations.";
                 public const string ParameterUnitIsUnspecified = "Parameter Unit is unspecified, Contact your Authority for assistance.";
-                public const string FlowResultQualifierMustBeEmpty = "Result Qualifier must be empty.";
+                public const string FlowResultQualifierMustBeEmpty = "Result Qualifier must be empty for the Mass Loading Flow parameter.";
 
                 public const string TranslatedUnitDoesNotSupportUnitConversion =
                     "Invalid unit translation. Cannot convert {0} to {1}.  Check the unit translation selected or contact your Authority.";
